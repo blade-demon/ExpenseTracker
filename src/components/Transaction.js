b315de0cffe1fd2975c519927ee427978d54cb20
blade-demon <xuziwei89@gmail.com>
2021-12-12 16:01:22 +0800

    complete hooks demo

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
-export const Transaction = ({ text, amount }) => {
+export const Transaction = ({ text, amount, id }) => {
   // Get sign
   const sign = amount < 0 ? '-' : '+'
-
+  const { deleteTransaction } = useContext(GlobalContext)
   return (
     <div>
       <li className={amount < 0 ? 'minus' : 'plus'}>
@@ -11,7 +12,9 @@ export const Transaction = ({ text, amount }) => {
         <span>
           {sign}${Math.abs(amount)}
         </span>
-        <button className="delete-btn">x</button>
+        <button className="delete-btn" onClick={() => deleteTransaction(id)}>
+          x
+        </button>
       </li>
     </div>
   )
